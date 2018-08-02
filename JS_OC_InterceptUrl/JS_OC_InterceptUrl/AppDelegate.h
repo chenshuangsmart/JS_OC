32b43611f68a21c8b3c061643b85d5f0097aec10
陈双 <chenshuang@banggood.com>
2018-08-02 12:15:02 +0800

    the intercept about js with oc
    the intercept about js with oc

---
+++ JS_OC_InterceptUrl/JS_OC_InterceptUrl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JS_OC_InterceptUrl
+//
+//  Created by cs on 2018/8/2.
+//  Copyright © 2018年 cs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JS_OC_InterceptUrl/JS_OC_InterceptUrl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JS_OC_InterceptUrl
+//
+//  Created by cs on 2018/8/2.
+//  Copyright © 2018年 cs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
