d99cd69baa3ea77149f4c7e145aa272e1ca1e14e
陈双 <chenshuang@banggood.com>
2018-08-18 11:48:16 +0800

    iOS下 JS 与 OC 互相调用- UIWebView+WebViewJavascriptBridge
    iOS下 JS 与 OC 互相调用- UIWebView+WebViewJavascriptBridge

--- UIWeb_WebViewJavascriptBridge/UIWeb_WebViewJavascriptBridge/WKBridgeViewController.m
+++ UIWeb_WebViewJavascriptBridge/UIWeb_WebViewJavascriptBridge/WKBridgeViewController.m
@@ -5,10 +5,18 @@
 //  Created by cs on 2018/8/17.
 //  Copyright © 2018年 cs. All rights reserved.
 //
-
 #import "WKBridgeViewController.h"
+#import <WKWebViewJavascriptBridge.h>
+#import <WebKit/WebKit.h>
 
-@interface WKBridgeViewController ()
+@interface WKBridgeViewController()<WKNavigationDelegate, WKUIDelegate>
+
+/** webView */
+@property(nonatomic, strong)WKWebView *webView;
+/** progress */
+@property (strong, nonatomic)UIProgressView *progressView;
+/** bridge */
+@property(nonatomic, strong)WKWebViewJavascriptBridge *webViewBridge;
 
 @end
 
@@ -17,21 +25,168 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setupInit];
+    [self drawUI];
+    // init progress view
+    [self initProgressView];
+    // 添加JS 要调用的Native 功能
+    [self registerNativeFunctions];
+    
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - setupInit
+
+- (void)setupInit {
+    self.title = @"WKWebView--WebViewJavascriptBridge";
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)dealloc {
+    NSLog(@"%s",__func__);
 }
-*/
+
+#pragma mark - drawUI
+
+- (void)drawUI {
+    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemReply target:self action:@selector(rightClick)];
+    self.navigationItem.rightBarButtonItem = rightItem;
+    
+    [self initWKWebView];
+    
+    // WebViewJavascriptBridge
+    self.webViewBridge = [WKWebViewJavascriptBridge bridgeForWebView:self.webView];
+    [self.webViewBridge setWebViewDelegate:self];
+}
+
+- (void)initWKWebView {
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    configuration.userContentController = [WKUserContentController new];
+    
+    WKPreferences *preferences = [WKPreferences new];
+    preferences.javaScriptCanOpenWindowsAutomatically = YES;
+    preferences.minimumFontSize = 30.0;
+    configuration.preferences = preferences;
+    
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:configuration];
+    [self.view addSubview:self.webView];
+    
+    NSString *urlStr = [[NSBundle mainBundle] pathForResource:@"index.html" ofType:nil];
+    NSString *localHtml = [NSString stringWithContentsOfFile:urlStr encoding:NSUTF8StringEncoding error:nil];
+    NSURL *fileURL = [NSURL fileURLWithPath:urlStr];
+    [self.webView loadHTMLString:localHtml baseURL:fileURL];
+    
+    self.webView.UIDelegate = self;
+    [self.view addSubview:self.webView];
+}
+
+- (void)initProgressView {
+    CGFloat kScreenWidth = [[UIScreen mainScreen] bounds].size.width;
+    UIProgressView *progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 2)];
+    progressView.tintColor = [UIColor redColor];
+    progressView.trackTintColor = [UIColor lightGrayColor];
+    [self.view addSubview:progressView];
+    
+    self.progressView = progressView;
+}
+
+#pragma mark - action
+
+- (void)rightClick
+{
+    //    // 如果不需要参数，不需要回调，使用这个
+    //    [_webViewBridge callHandler:@"testJSFunction"];
+    //    // 如果需要参数，不需要回调，使用这个
+    //    [_webViewBridge callHandler:@"testJSFunction" data:@"一个字符串"];
+    // 如果既需要参数，又需要回调，使用这个
+    [_webViewBridge callHandler:@"testJSFunction" data:@"一个字符串" responseCallback:^(id responseData) {
+        NSLog(@"调用完JS后的回调：%@",responseData);
+    }];
+}
+
+#pragma mark - private function
+
+- (void)registerNativeFunctions {
+    [self registShareFunction];
+    
+    [self registLocationFunction];
+    
+    [self registPayFunction];
+}
+
+- (void)registShareFunction {
+    [self.webViewBridge registerHandler:@"shareClick" handler:^(id data, WVJBResponseCallback responseCallback) {
+        // data 的类型与 JS中传的参数有关
+        NSDictionary *tempDic = data;
+        // 在这里执行分享的操作
+        NSString *title = [tempDic objectForKey:@"title"];
+        NSString *content = [tempDic objectForKey:@"content"];
+        NSString *url = [tempDic objectForKey:@"url"];
+        NSLog(@"JS 传递给 OC 的参数:%@",[NSString stringWithFormat:@"分享成功:%@,%@,%@",title,content,url]);
+        
+        // 将分享的结果返回到JS中
+        NSString *result = [NSString stringWithFormat:@"分享成功:%@,%@,%@",title,content,url];
+        responseCallback(result);
+    }];
+}
+
+- (void)registLocationFunction {
+    [self.webViewBridge registerHandler:@"locationClick" handler:^(id data, WVJBResponseCallback responseCallback) {
+        // 获取位置信息
+        
+        // 将结果返回给 JS
+        NSString *location = @"广东省广州市白云区豪泉大厦";
+        responseCallback(location);
+    }];
+}
+
+- (void)registPayFunction {
+    [self.webViewBridge registerHandler:@"payClick" handler:^(id data, WVJBResponseCallback responseCallback) {
+        // data 的类型与 JS中传的参数有关
+        NSDictionary *tempDic = data;
+        
+        NSString *orderNo = [tempDic objectForKey:@"order_no"];
+        long long amount = [[tempDic objectForKey:@"amount"] longLongValue];
+        NSString *subject = [tempDic objectForKey:@"subject"];
+        NSString *channel = [tempDic objectForKey:@"channel"];
+        // 支付操作...
+        NSLog(@"支付操作:%@", [NSString stringWithFormat:@"支付成功:%@,%@,%@,%ld",orderNo,subject,channel,amount]);
+        
+        // 将分享的结果返回到JS中
+        NSString *result = [NSString stringWithFormat:@"支付成功:%@,%@,%@",orderNo,subject,channel];
+        responseCallback(result);
+    }];
+}
+
+#pragma mark - KVO
+
+// 计算wkWebView进度条
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (object == self.webView && [keyPath isEqualToString:@"estimatedProgress"]) {
+        CGFloat newprogress = [[change objectForKey:NSKeyValueChangeNewKey] doubleValue];
+        if (newprogress == 1) {
+            [self.progressView setProgress:1.0 animated:YES];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressView.hidden = YES;
+                [self.progressView setProgress:0 animated:NO];
+            });
+        } else {
+            self.progressView.hidden = NO;
+            [self.progressView setProgress:newprogress animated:YES];
+        }
+    }
+}
+
+
+#pragma mark - WKUIDelegate
+
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提醒" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
